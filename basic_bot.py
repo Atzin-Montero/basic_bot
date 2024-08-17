db1d64e6811c0793d950597c3ea1056179096959
Atzin-Montero <a23300709@ceti.mx>
2024-08-17 15:06:49 -0600

    basic_bot.py

--- basic_bot.py
+++ basic_bot.py
@@ -16,7 +16,7 @@ async def on_ready():
 @bot.command()
 async def help_me(ctx):
     await ctx.send("Aquí tienes una lista de comandos (N significa un número): ")
-    await ctx.send("$hello \n$heh N\n$flip_coin\n$gen_emoji\n$add N N\n$gen_pass\n$roll NdN\n$repeat N palabra\n$choose opcion1 opcion2 (...)\n$mem\n$random_mem\n$duck")
+    await ctx.send("$hello \n$heh N\n$flip_coin\n$gen_emoji\n$add N N\n$rest N N\n$multiply N N\n$divide N N\n$gen_pass\n$roll NdN\n$repeat N palabra\n$choose opcion1 opcion2 (...)\n$mem\n$random_mem\n$duck\n$huerto_urbano")
 
 @bot.command()
 async def hello(ctx):
@@ -113,4 +113,40 @@ async def duck(ctx):
     image_url = get_duck_image_url()
     await ctx.send(image_url)
 
+@bot.command()
+async def huerto_urbano(ctx):
+    await ctx.send(f"""Hola, soy un bot {bot.user}!""")
+    await ctx.send('Te voy hablar un poco sobre el cultivo de plantas urbano')
+    time.sleep(1)
+    await ctx.send('El cultivo urbano de plantas es una forma innovadora de conectar con la naturaleza en medio de la ciudad.')
+    time.sleep(2)
+    await ctx.send('Aprovechando espacios pequeños, como balcones y azoteas, podemos crear jardines que no solo embellecen el entorno, sino que también contribuyen a mejorar la calidad del aire y a producir alimentos frescos.')
+    time.sleep(2)
+    await ctx.send("Es una práctica que promueve la autosuficiencia y el bienestar, transformando rincones urbanos en oasis verdes.")
+    time.sleep(3)
+    await ctx.send("Quieres saber las ventajas de tener cultivos urbanos?, Responde con 'sí' o 'no'.")
+    def check(message):
+        return message.author == ctx.author and message.channel == ctx.channel and message.content in ['sí', 'si', 'no']
+    response = await bot.wait_for('message', check=check)
+    if response:
+        if response.content in ['sí', 'si']:
+            await ctx.send("1. Mejora la calidad del aire.")
+            await ctx.send("2. Proporciona alimentos frescos.")
+            await ctx.send("3. Reduce el estrés y mejora el bienestar.")   
+        else:
+            await ctx.send("Está bien, si alguna vez te da curiosidad, no dudes en preguntar.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+    await ctx.send("Quieres consejos sobre cómo cultivar?, responde si o no")
+    response1 = await bot.wait_for('message', check=check)
+    if response1:
+        if response1.content in ['sí', 'si']:
+            await ctx.send("1. Aprovecha espacios verticales para maximizar el área disponible.")
+            await ctx.send("2. Utiliza contenedores y macetas con buen drenaje.")
+            await ctx.send("3. Selecciona plantas adaptadas a las condiciones urbanas, como lechugas, rábanos, chícharos, etc.")
+        else:
+            await ctx.send("Está bien, si alguna vez necesitas consejos, no dudes en preguntar.")
+    else:
+        await ctx.send("Lo siento, no pude entender tu respuesta. Inténtalo de nuevo.")
+        
 bot.run("TOKEN")
